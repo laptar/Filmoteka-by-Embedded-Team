6e3b723f41111c38858094a862c5900d59891da8
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-07-05 18:03:26 +0300

    modal window raw version

---
+++ src/JS/Crew/crew-list.js
@@ -0,0 +1,45 @@
+import crewMembers from './crew-members';
+import teamCard from '../../Templates/teamCard.hbs';
+
+const teamContainer = document.querySelector('.team-members');
+const openModalTeam = document.querySelector('.js-open-modal');
+const closeModalTeam = document.querySelector('.team-close');
+const backdropModalTeam = document.querySelector('.team-backdrop');
+const modalTeam = document.querySelector('[data-team]');
+
+openModalTeam.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  console.log(modalTeam.classList);
+  modalTeam.classList.remove('is-hidden');
+  renderTeamList(crewMembers);
+  window.addEventListener('keydown', onEscPress);
+  closeModalTeam.addEventListener('click', onClose);
+  backdropModalTeam.addEventListener('click', onBackdrop);
+}
+
+function renderTeamList() {
+  const markup = teamCard(crewMembers);
+  teamContainer.innerHTML = markup;
+}
+
+function onClose() {
+  modalTeam.classList.add('is-hidden');
+  window.removeEventListener('keydown', onEscPress);
+  closeModalTeam.removeEventListener('click', onClose);
+  backdropModalTeam.removeEventListener('click', onBackdrop);
+  teamContainer.innerHTML = '';
+}
+
+function onBackdrop(event) {
+  if (event.currentTarget === event.target) {
+    onClose();
+  }
+}
+
+function onEscPress(event) {
+  if (event.code === 'Escape') {
+    onClose();
+  }
+}
