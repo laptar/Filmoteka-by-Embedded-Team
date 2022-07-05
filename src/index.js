787c1929b07bac680da2f800dfd83eec98235611
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-07-05 16:42:19 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import fetchPopular from './JS/fetchPopular';
 import searchMovie from './JS/searchMovie';
 import './sass/main.scss';
+
+import { renderModal } from './JS/modal_close';
 const list = document.querySelector('.gallery__list');
 const form = document.querySelector('.search');
 const warning = document.querySelector('.warning');
@@ -38,18 +40,18 @@ function onFormSubmit(e) {
 }
 
 // Функция загружает популярные фильмы на главную страницу
-renderPopular();
-function renderPopular() {
-  fetchPopular().then(data => {
-    const popular = data.results;
-    const markup = popular.map(movie => {
-      const releaseDate = movie.release_date.split('-')[0];
-      return `<li class="gallery__item">
+let popular;
+
+fetchPopular().then(data => {
+  popular = data.results;
+  console.log(popular);
+  const markup = popular.map(movie => {
+    const releaseDate = movie.release_date.split('-')[0];
+    return `<li class="gallery__item">
                             <img class="gallery__image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
                             <h3 class="gallery__film-title">${movie.title}</h3>
                             <p class="gallery__film-genres"><span class="gallery__film-year">${releaseDate}</span></p>
                     </li>`;
-    });
-    list.innerHTML = markup.join('');
   });
-}
+  list.innerHTML = markup.join('');
+});
--- src/js/modal_close.js
+++ src/js/modal_close.js
@@ -1,15 +1,17 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// function renderModal(arr) {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+//   {
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//     console.log(arr);
+//     // console.dir(event.target);
+//     // refs.modal.classList.toggle('is-hidden');
+//   }
+// }
 
-  function toggleModal(event) {
-    console.dir(event.target);
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+// export { renderModal };
