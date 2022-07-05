39ca6191c68b5ec27590442a85d0607b23859a86
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-07-05 13:29:36 +0300

    1

--- src/JS/fetchPopular.js
+++ src/JS/fetchPopular.js
@@ -1,12 +1,13 @@
-const API_KEY = "74df5c4b3da0f8bb23d044877e91bb86";
-const API_URL_TRENDING = "https://api.themoviedb.org/3/trending/";
-const MEDIA_TYPE = "movie";
-const TIME_WINDOW = "day";
+const API_KEY = '74df5c4b3da0f8bb23d044877e91bb86';
+const API_URL_TRENDING = 'https://api.themoviedb.org/3/trending/';
+const MEDIA_TYPE = 'movie';
+const TIME_WINDOW = 'day';
 let page = 1;
 
-
 export default function fetchPopular() {
-    return fetch(`${API_URL_TRENDING}${MEDIA_TYPE}/${TIME_WINDOW}?api_key=${API_KEY}&page=${page}`).then(response => {
-        return response.json()
-    })
+  return fetch(
+    `${API_URL_TRENDING}${MEDIA_TYPE}/${TIME_WINDOW}?api_key=${API_KEY}&page=${page}`
+  ).then(response => {
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -10,10 +10,9 @@ function renderPopular() {
     const markup = popular.map(movie => {
       const releaseDate = movie.release_date.split('-')[0];
       return `<li class="gallery__item">
-                           <a><img class="gallery__image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+                           <img class="gallery__image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
                             <h3 class="gallery__film-title">${movie.title}</h3>
                             <p class="gallery__film-genres"><span class="gallery__film-year">${releaseDate}</span></p>
-                           </a> 
                     </li>`;
     });
     list.innerHTML = markup.join('');
