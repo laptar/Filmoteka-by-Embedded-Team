92466418373c6c7b7b9cd9ce847e774e8dc13dca
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-07-05 16:45:08 +0300

    1

--- src/js/modal_close.js
+++ src/js/modal_close.js
@@ -1,17 +1,15 @@
-// function renderModal(arr) {
+// (() => {
 //   const refs = {
 //     openModalBtn: document.querySelector('[data-modal-open]'),
 //     closeModalBtn: document.querySelector('[data-modal-close]'),
 //     modal: document.querySelector('[data-modal]'),
 //   };
-//   {
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//     console.log(arr);
-//     // console.dir(event.target);
-//     // refs.modal.classList.toggle('is-hidden');
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal(event) {
+//     console.dir(event.target);
+//     refs.modal.classList.toggle('is-hidden');
 //   }
-// }
-
-// export { renderModal };
+// })();
