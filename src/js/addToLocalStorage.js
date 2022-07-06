127d27cf409ba5992ab7a79b8ffb535c484c3acd
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-07-06 13:31:16 +0300

    1

--- src/js/addToLocalStorage.js
+++ src/js/addToLocalStorage.js
@@ -1,13 +1,26 @@
-
-
-function addToLocalStorage(fetch, fetchGenres, movie = "") {
-    fetch(movie).then(data => {
-        localStorage.setItem('popular', JSON.stringify(data.results));
-    });
-
+function addToLocalStorage(fetch, fetchGenres, movie = '') {
+  fetch(movie).then(cards => {
+    // console.log(card.results);
     fetchGenres().then(data => {
-        localStorage.setItem('genres', JSON.stringify(data.genres));
+      console.log(data.genres);
+      console.log(cards.results);
+      const cardGanr = cards.results;
+      cards.results.forEach(card => {
+        // console.log(card.genre_ids);
+        card.genre_ids.forEach(id => {
+          console.log(id);
+        });
+      });
+      console.log(cardGanr);
+
+      //   localStorage.setItem('genres', JSON.stringify(data.genres));
     });
+    // localStorage.setItem('popular', JSON.stringify(data.results));
+  });
+
+  fetchGenres().then(data => {
+    localStorage.setItem('genres', JSON.stringify(data.genres));
+  });
 }
 
-export { addToLocalStorage }
+export { addToLocalStorage };
