66976801be8a157caaa322434b4ae63107bc1273
Roman Babiy <romchik.babiy@gmail.com>
2022-07-09 09:08:45 +0300

    upd

--- src/library.js
+++ src/library.js
@@ -1,3 +1,4 @@
+import './js/preloader';
 import './js/crew1/crew-list';
 import './js/scroll-up.js';
 import './sass/main.scss';
@@ -11,51 +12,54 @@ const queue = document.querySelector('.queue');
 const libList = document.querySelector('.gallery-library__list');
 
 function addArrToLocalStor(key) {
-  return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
+    return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
 }
 let wachedArray = addArrToLocalStor('watched');
 let queueArray = addArrToLocalStor('queue');
 
-let currentList = sessionStorage.getItem('currentList')
-  ? JSON.parse(sessionStorage.getItem('currentList'))
-  : 'watched';
+let currentList = sessionStorage.getItem('currentList') ?
+    JSON.parse(sessionStorage.getItem('currentList')) :
+    'watched';
 
 if (currentList === 'watched') {
-  watched.classList.add('btn-active');
-  queue.classList.remove('btn-active');
-  console.log(wachedArray);
-  libList.innerHTML = renderMovieCard(wachedArray);
+    watched.classList.add('btn-active');
+    queue.classList.remove('btn-active');
+    console.log(wachedArray);
+    libList.innerHTML = renderMovieCard(wachedArray);
 } else {
-  watched.classList.remove('btn-active');
-  queue.classList.add('btn-active');
-  libList.innerHTML = renderMovieCard(queueArray);
+    watched.classList.remove('btn-active');
+    queue.classList.add('btn-active');
+    libList.innerHTML = renderMovieCard(queueArray);
 }
 
 queue.addEventListener('click', clickBtn);
+
 function clickBtn() {
-  watched.classList.remove('btn-active');
-  queue.classList.add('btn-active');
-  sessionStorage.setItem('currentList', JSON.stringify('queue'));
-  currentList = 'queue';
-  queueArray = addArrToLocalStor('queue');
-  libList.innerHTML = renderMovieCard(queueArray);
+    watched.classList.remove('btn-active');
+    queue.classList.add('btn-active');
+    sessionStorage.setItem('currentList', JSON.stringify('queue'));
+    currentList = 'queue';
+    queueArray = addArrToLocalStor('queue');
+    libList.innerHTML = renderMovieCard(queueArray);
 }
 
 watched.addEventListener('click', clickBtnWatch);
+
 function clickBtnWatch() {
-  watched.classList.add('btn-active');
-  queue.classList.remove('btn-active');
-  sessionStorage.setItem('currentList', JSON.stringify('watched'));
-  currentList = 'watched';
-  wachedArray = addArrToLocalStor('watched');
-  libList.innerHTML = renderMovieCard(wachedArray);
+    watched.classList.add('btn-active');
+    queue.classList.remove('btn-active');
+    sessionStorage.setItem('currentList', JSON.stringify('watched'));
+    currentList = 'watched';
+    wachedArray = addArrToLocalStor('watched');
+    libList.innerHTML = renderMovieCard(wachedArray);
 }
 
 libList.addEventListener('click', clickCard);
+
 function clickCard(evt) {
-  if (currentList === 'watched') {
-    renderModal(evt, 'watched');
-  } else {
-    renderModal(evt, 'queue');
-  }
+    if (currentList === 'watched') {
+        renderModal(evt, 'watched');
+    } else {
+        renderModal(evt, 'queue');
+    }
 }
