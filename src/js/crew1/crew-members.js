9e2f03f6ddc776bf1a25e3f9a6162c6c9917dbec
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-07-10 14:50:03 +0300

    add our leader picture

--- src/js/crew1/crew-members.js
+++ src/js/crew1/crew-members.js
@@ -1,7 +1,6 @@
-import TarasLap from '../../images/team-pictures/Taras-Lapitskiy.png';
+import TarasLap from '../../images/team-pictures/Taras-Lapitskiy.jpg';
 import RomanBabiy from '../../images/team-pictures/Roman-babiy.jpg';
 import IrynaSabinina from '../../images/team-pictures/IrynaSabinina.jpg';
-import DmytroShynkarenko from '../../images/team-pictures/DmytroShynkarenko.jpg';
 import AndriyHrebenozhko from '../../images/team-pictures/Andriy-Hrebenozhko.jpg';
 import YehorBoichenko from '../../images/team-pictures/Yehor-Boichenko.png';
 
